e609a56cf50788550d9970caadbdb6dffd1a8edd
zhenyan.guo <user@example.com>
2025-01-24 13:10:42 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
+  server: {
+    port: 3000,
+    host: true
+  },
   plugins: [vue()],
   resolve: {
     alias: {
